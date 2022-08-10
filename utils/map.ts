a124417ae85462edbb18ef65df4d0e5d7c4fab4a
Ivan Khanevich <mail@iamkhan.tech>
2022-08-10 22:17:50 +0400

    style: improve selection style

--- utils/map.ts
+++ utils/map.ts
@@ -31,13 +31,10 @@ export function initiateMap(
 export function addMapDraw(map: Map) {
   const draw = new MapboxDraw({
     displayControlsDefault: false,
-    // Select which mapbox-gl-draw control buttons to add to the map.
     controls: {
       polygon: true,
       trash: true,
     },
-    // Set mapbox-gl-draw to draw by default.
-    // The user does not have to click the polygon control button first.
     defaultMode: "simple_select",
     styles: drawStyles,
   });
