a5c78c054e8cb84aa6723f05a125a3e0b57b20e7
Ivan Khanevich <mail@iamkhan.tech>
2022-08-12 19:11:28 +0400

    feat: updates

--- components/home/SaveCoverage.tsx
+++ components/home/SaveCoverage.tsx
@@ -8,11 +8,31 @@ import { useStore } from "@nanostores/react";
 import { $loader } from "@store/loader";
 
 const SaveCoverage = () => {
+  const coverageLoaded = React.useRef(false);
   const loader = useStore($loader);
   const coverage = useStore($selectedCoverage);
+  const [isCoverageUpdated, setIsCoverageUpdated] = React.useState(false);
+
+  React.useEffect(() => {
+    if (coverage) {
+      if (coverageLoaded.current) {
+        setIsCoverageUpdated(true);
+      } else {
+        coverageLoaded.current = true;
+      }
+    }
+  }, [coverage]);
+
+  function save() {
+    saveSelectedCoverage();
+    setIsCoverageUpdated(false);
+  }
 
   return (
-    <Button onClick={saveSelectedCoverage} disabled={!!loader || !coverage}>
+    <Button
+      onClick={save}
+      disabled={!!loader || !coverage || !isCoverageUpdated}
+    >
       Save coverage area
     </Button>
   );
